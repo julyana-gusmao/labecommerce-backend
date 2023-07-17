3ed2a46acd3798715a278184263b54380577e78c
Julyana Gusmao <the2kira@hotmail.com>
2023-07-17 16:02:04 -0300

    Projeto finalizado e adicionado o arquivo README contendo o link da API

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -57,6 +57,7 @@ CREATE TABLE
     );
 
 
+DROP TABLE products;
 ----------- Purchases
 
 CREATE TABLE
@@ -81,6 +82,7 @@ CREATE TABLE
         product_id TEXT NOT NULL,
         quantity INT NOT NULL,
         FOREIGN KEY (product_id) REFERENCES products(id)
+        FOREIGN KEY (purchase_id) REFERENCES purchases(id)
         ON UPDATE CASCADE -- efeito cascata ao atualizar id na tabela users
 		ON DELETE CASCADE -- efeito cascata ao atualizar id na tabela users
     );
@@ -89,129 +91,117 @@ CREATE TABLE
 DROP TABLE purchases_products;
 
 DROP TABLE products;
--------------------- INSERTS -------------------
-INSERT INTO
-    users (
-        id,
-        name,
-        email,
-        password
-    )
-VALUES (
-        'u001',
-        'Fayra',
-        'fayralinda@gmail.com',
-        '123321'
-    ), (
-        'u002',
-        'Alexia',
-        'alexiacris@gmail.com',
-        '1232314'
-    ), (
-        'u003',
-        'Lucca',
-        'luquinha2000@hotmail.com',
-        'sr195812'
-    );
+-- -------------------- INSERTS -------------------
+-- INSERT INTO
+--     users (
+--         id,
+--         name,
+--         email,
+--         password
+--     )
+-- VALUES (
+--         'u001',
+--         'Fayra',
+--         'fayralinda@gmail.com',
+--         '123321'
+--     ), (
+--         'u002',
+--         'Alexia',
+--         'alexiacris@gmail.com',
+--         '1232314'
+--     ), (
+--         'u003',
+--         'Lucca',
+--         'luquinha2000@hotmail.com',
+--         'sr195812'
+--     );
 
 
-INSERT INTO
-    products (
-        id,
-        name,
-        price,
-        description,
-        image_url
-    )
-VALUES (
-        'p001',
-        'Monitor Gamer',
-        1300,
-        'Lorem Ipsum Lorem',
-        'https://picsum.photos/200'
-    ), (
-        'p002',
-        'Teclado Gamer',
-        400,
-        'Lorem Ipsum Lorem',
-        'https://picsum.photos/200'
-    ), (
-        'p003',
-        'Mousepad RGB',
-        180,
-        'Lorem Ipsum Lorem',
-        'https://picsum.photos/200'
-    ), (
-        'p004',
-        'Controle XBOX',
-        500,
-        'Lorem Ipsum Lorem',
-        'https://picsum.photos/200'
-    ), (
-        'p005',
-        'Cadeira Gamer',
-        1500,
-        'Lorem Ipsum Lorem',
-        'https://picsum.photos/200'
-    );
+-- INSERT INTO
+--     products (
+--         id,
+--         name,
+--         price,
+--         description,
+--         image_url
+--     )
+-- VALUES (
+--         'p001',
+--         'Monitor Gamer',
+--         1300,
+--         'Lorem Ipsum Lorem',
+--         'https://picsum.photos/200'
+--     ), (
+--         'p002',
+--         'Teclado Gamer',
+--         400,
+--         'Lorem Ipsum Lorem',
+--         'https://picsum.photos/200'
+--     ), (
+--         'p003',
+--         'Mousepad RGB',
+--         180,
+--         'Lorem Ipsum Lorem',
+--         'https://picsum.photos/200'
+--     ), (
+--         'p004',
+--         'Controle XBOX',
+--         500,
+--         'Lorem Ipsum Lorem',
+--         'https://picsum.photos/200'
+--     ), (
+--         'p005',
+--         'Cadeira Gamer',
+--         1500,
+--         'Lorem Ipsum Lorem',
+--         'https://picsum.photos/200'
+--     );
 
 
 INSERT INTO
     purchases (
         id,
         buyer,
-        total_price,
-        created_at
+        total_price
     )
 VALUES (
         "c001",
-        "u001",
-        400.3,
-        "26/03/2023"
-    ), (
-        "c002",
         "u002",
-        700.2,
-        "26/03/2023"
-    ), (
-        "c003",
-        "u001",
-        200.9,
-        "29/03/2023"
+        400.3
     );
 
 
-    INSERT INTO purchases_products (purchase_id, product_id, quantity)
-    VALUES ("c001", "p001", 1), ("c002", "p002", 1), ("c002", "p004", 2);
+--     INSERT INTO purchases_products (purchase_id, product_id, quantity)
+--     VALUES ("c001", "p001", 1), ("c002", "p002", 1), ("c002", "p004", 2);
 
--------- Setting the total price as soon as enraised
+-- -------- Setting the total price as soon as enraised
 
-UPDATE purchases
-SET total_price = 900
-WHERE id = "c002";
+-- UPDATE purchases
+-- SET total_price = 900
+-- WHERE id = "c002";
 
-------------- QUERIES ADD AND DELETE
+-- ------------- QUERIES ADD AND DELETE
 
-INSERT INTO
-    users (
-        id,
-        name,
-        email,
-        password
-    );
+-- INSERT INTO
+--     users (
+--         id,
+--         name,
+--         email,
+--         password
+--     );
 
-INSERT INTO products (id, name, price, description, image_url);
+-- INSERT INTO products (id, name, price, description, image_url);
 
-DELETE FROM users
-WHERE id = ''
-DELETE FROM products
-WHERE id = '';
+-- DELETE FROM users
+-- WHERE id = ''
+-- DELETE FROM products
+-- WHERE id = '';
 
-UPDATE products
-SET
-    id = '',
-    name = '',
-    price = '',
-    description = '',
-    image_url = ''
-WHERE id = '';
+-- UPDATE products
+-- SET
+--     id = '',
+--     name = '',
+--     price = '',
+--     description = '',
+--     image_url = ''
+-- WHERE id = '';
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { users, products } from "./database";
 import express, { Request, Response } from "express";
 import cors from "cors";
-import { TProducts, TUsers } from "./types";
+
+import { db } from "./database/knex";
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -12,121 +13,445 @@ app.listen(3003, () => {
 })
 
 
-////ENDPOINTS
+//////////////////// GET //////////////////////
 
-app.get("/users", (req: Request, res: Response) => {
-    res.status(200).send(users)
-})
 
-app.get("/products", (req: Request, res: Response) => {
-    res.status(200).send(products)
-})
+app.get('/users', async (req: Request, res: Response) => {
 
-//Endpoint para procurar um produto pelo nome
-app.get("/products", (req: Request, res: Response) => {
-    const q = req.query.q as string
-    const result: TProducts[] = products.filter((product) => { return product.name.toLowerCase().includes(q.toLowerCase()) })
+    try {
 
-    if (result.length >= 1) {
+           // const result = await db.select("*").from("users")
+        const result = await db("users")
+
         res.status(200).send(result)
-    } else {
-        res.status(200).send(products)
-    }
-});
 
-//Endpoint para criar um usuário novo
-app.post("/users", (req: Request, res: Response) => {
-
-    const id = req.body.id as string
-    const name = req.body.name as string
-    const email = req.body.email as string
-    const password = req.body.password as string
-    const createdAt = new Date().toISOString() as string
-
-    const newUser: TUsers = {
-        id,
-        name,
-        email,
-        password,
-        createdAt
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
     }
 
-    users.push(newUser)
-    res.status(201).send("Cadastro realizado com sucesso")
 
 })
 
-//Endpoint para criar um produto novo
-app.post("/products", (req: Request, res: Response) => {
+app.get('/products', async (req: Request, res: Response) => {
+
+    try {
+        // const result = await db.select("*").from("products")
+        const result = await db("products")
+       
+        res.status(200).send(result)
+
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
+    }
+})
+
+
+app.get('/purchases', async (req: Request, res: Response) => {
+
+    try {
+        // const result = await db.select("*").from("products")
+        const result = await db("purchases")
+       
+        res.status(200).send(result)
+
+    }catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
+    }
+
+})
+
+// GET PURCHASE BY ID
+
+app.get("/purchases/:id", async (req: Request, res: Response) => {
+
+    try {
+        const id = req.params.id
+
+        const [purchase] = await db("purchases")
+        .select(
+          "purchases.id as purchaseId",
+          "purchases.buyer as purchaseBuyer",
+          "users.name as buyerName",
+          "users.email as buyerEmail",
+          "purchases.total_price",
+          "purchases.created_at",
+  
+        )
+        .innerJoin("users", "purchases.buyer", "=", "users.id").where("purchases.id", "=", id)
+
+
+        if(!purchase){
+            res.status(400)
+            throw new Error("Id não encontrado")
+        }
+
+        const resultPurchaseProduct = await db("purchases_products").select(
+            "products.id as productId",
+            "products.name",
+            "products.price",
+            "products.description",
+            "products.image_url",
+            "purchases_products.quantity"
+          ).innerJoin("products", "purchases_products.product_id", "products.id").where("purchases_products.purchase_id", "=", id)
+
+
+          const result = {
+            ...purchase,
+            products: resultPurchaseProduct
+          }
+
+        res.status(200).send(result)
+            
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
+    }
+})
+
+// GET PRODUCT BY NAME -- PRECISA DO /search **
+
+// app.get('/products/search', async (req: Request, res: Response) => {
+
+    app.get('/products/search', async (req: Request, res: Response) => {
+
+    try {
+
+        const q = req.query.q as string
+        const [ product ] = await db("products").where( "name", "LIKE", `%${q}%` )
+
+        if (!q){
+            res.status(400)
+            throw new Error("Escreva um produto a ser procurado")
+        }
+
+        res.status(200).send(product)
+
+        // const q = req.query.q as string
+
+        // const [product] = await db.raw(`
+        // SELECT * FROM products
+        // WHERE name LIKE "%${q}%";
+        // `)
+
+        // if (!product) {
+        //     res.status(400)
+        //     throw new Error("Produto não encontrado")
+        // }
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
+    }
+
+})
+
+////CREATE USERS
+
+app.post("/users", async (req: Request, res: Response) => {
+
+    try {
+        
+        const id = req.body.id as string
+        const name = req.body.name as string
+        const email = req.body.email as string
+        const password = req.body.password as string
+
+
+        if(typeof id !== "string"){
+            res.status(400)
+            throw new Error("'id' precisa ser tipo texto")
+        }
+
+        if(typeof name !== "string"){
+            res.status(400)
+            throw new Error("'name' precisa ser tipo texto")
+        }
+
+        if(typeof email !== "string"){
+            res.status(400)
+            throw new Error("'email' precisa ser tipo texto")
+        }
+
+        if(typeof password !== "string"){
+            res.status(400)
+            throw new Error("'password' precisa ser tipo texto")
+        }
+
+
+        if(id.length < 1 || name.length < 1){
+            res.status(400)
+            throw new Error ("'id' e 'nome' devem ter mais de 2 caracteres")
+        }
+
+        const newUser = {
+            id: id,
+            name: name, 
+            email: email,
+            password: password
+        }
+
+        await db("users").insert(newUser)
+
+        res.status(201).send("Cadastro realizado com sucesso")
+
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
+    }
+})
+
+////CREATE PRODUCT
+
+app.post("/products", async (req: Request, res: Response) => {
+
+    try {
 
     const id = req.body.id as string
     const name = req.body.name as string
     const price = req.body.price as number
     const description = req.body.description as string
-    const imageUrl = req.body.imageUrl as string
+    const image_url = req.body.image_url as string
 
-    const newProduct: TProducts = {
-        id,
-        name,
-        price,
-        description,
-        imageUrl
+    if(typeof id !== "string"){
+        res.status(400)
+        throw new Error("'id' precisa ser tipo texto")
     }
 
-    products.push(newProduct)
-    res.status(201).send("Produto cadastrado com sucesso")
-
-})
-
-//Endpoint para deletar um USUÁRIO por id
-app.delete("/users/:id", (req: Request, res: Response) => {
-    const id = req.params.id
-    // encontrar o index do item que será removido
-    const userIndex = users.findIndex((user) => user.id === id)
-
-    // Se o item existir, o index será maior ou igual a 0
-    if (userIndex >= 0) {
-        users.splice(userIndex, 1)
+    if(typeof name !== "string"){
+        res.status(400)
+        throw new Error("'name' precisa ser tipo texto")
     }
 
-    res.status(200).send("Usuário deletado com sucesso")
-})
-
-
-//Endpoint para deletar um PRODUTO por id
-app.delete("/products/:id", (req: Request, res: Response) => {
-    const id = req.params.id
-    const productIndex = products.findIndex((product) => product.id === id)
-
-    if (productIndex >= 0) {
-        products.splice(productIndex, 1)
+    if(typeof price !== "number"){
+        res.status(400)
+        throw new Error("'price' precisa ser tipo número")
     }
 
-    res.status(200).send("Produto deletado com sucesso")
-})
+    if(typeof description !== "string"){
+        res.status(400)
+        throw new Error("'description' precisa ser tipo texto")
+    }
 
-//Endpoint para EDITAR um produto por id
-app.put("/products/:id", (req: Request, res: Response) => {
+    if(typeof image_url !== "string"){
+        res.status(400)
+        throw new Error("'image_url' precisa ser tipo texto")
+    }
+
+    if(id.length < 1 || name.length < 1){
+        res.status(400)
+        throw new Error ("'id' e 'nome' devem ter mais de 2 caracteres")
+    }
+
     
-    const id = req.params.id
+    const newProduct = {
+        id: id,
+        name: name,
+        price: price,
+        description: description,
+        image_url: image_url
+    }
 
+    await db("products").insert(newProduct)
+    // const newProduct = await db.raw(`
+    // INSERT INTO products (id, name, price, description, image_url)
+    // VALUES ("${id}", "${name}", ${price}, "${description}", "${image_url}")
+    // `)
+
+    res.status(201).send("Produto cadastrado com sucesso")
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
+    }
+    });
+
+
+/// CREATE PURCHASE
+
+app.post('/purchases', async (req: Request, res: Response) => {
+
+    try {
+
+        const id = req.body.id as string
+        const buyer = req.body.buyer as string
+        const totalPrice = req.body.totalPrice as number
+
+        if(typeof id !== "string"){
+            res.status(400)
+            throw new Error("'id' precisa ser tipo texto")
+        }
+
+        if(typeof buyer !== "string"){
+            res.status(400)
+            throw new Error("'buyer' precisa ser tipo texto")
+        }
+
+        if(typeof totalPrice !== "number"){
+            res.status(400)
+            throw new Error("'totalPrice' precisa ser tipo número")
+        }
+
+        if(id.length < 1 || buyer.length < 1){
+            res.status(400)
+            throw new Error ("'id' e 'buyer' devem ter mais de 2 caracteres")
+        }
+
+        const newPurchase = {
+            id: id,
+            buyer: buyer,
+            totalPrice: totalPrice
+        }
+
+        await db("purchases").insert(newPurchase)
+
+        res.status(201).send("Compra cadastrada com sucesso")
+
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
+    }
+});
+
+
+//////DELETE USER BY ID
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+
+    try{
+    const idToDelete = req.params.id
+    const [ user ] = await db("users").where({id: idToDelete})
+
+    if(!user){
+        res.status(400)
+        throw new Error("Id não encontrado")
+    }
+
+    await db("users").del().where({id: idToDelete})
+    res.status(200).send("Usuário deletado com sucesso")
+    
+    }catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
+    }})
+
+
+//DELETE PRODUCT BY ID
+
+app.delete("/products/:id", async (req: Request, res: Response) =>{
+    
+    try {
+        const idToDelete = req.params.id
+        const [ product ] = await db("products").where({id: idToDelete})
+
+        if(!product){
+            res.status(400)
+            throw new Error ("Id não encontrado")
+        }
+
+        await db("products").del().where({id: idToDelete})
+        res.status(200).send("Produto deletado com sucesso")
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
+    }
+})
+
+//PUT PRODUCT BY ID (EDIT)
+
+app.put("/products/:id", async (req: Request, res: Response) => {
+
+    try {
+
+    const idToEdit = req.params.id
     const newId = req.body.id as string | undefined
     const newName = req.body.name as string | undefined
-    const newPrice = req.body.price as number | undefined
+    const newPrice = req.body.price as number
     const newDescription = req.body.description as string | undefined
-    const newImageUrl = req.body.imageUrl as string | undefined
+    const newImage_Url = req.body.image_url as string | undefined
 
-    const productToEdit = products.find((product) => product.id === id)
+    const [ product ] = await db("products").where({ id: idToEdit })
 
-    if(productToEdit){
-        productToEdit.id = newId || productToEdit.id
-        productToEdit.name = newName || productToEdit.name
-        productToEdit.description = newDescription || productToEdit.description
-        productToEdit.imageUrl = newImageUrl || productToEdit.imageUrl
-
-        productToEdit.price = isNaN(newPrice) ? productToEdit.price : newPrice 
+    if(!product){
+        res.status(400)
+        throw new Error ("Id não encontrado")
     }
 
-    res.status(200).send("Atualização realizada com sucesso")
+    const updatedProduct = {
+        id: newId || product.id,
+        name: newName || product.name,
+        price: isNaN(newPrice) ? product.price : newPrice,
+        description: newDescription || product.description,
+        image_url: newImage_Url || product.image_url
+    }
+
+    await db("products").update(updatedProduct).where({id: idToEdit})
+                
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
+    }
+
+    res.status(200).send({ message: "Atualização realizada com sucesso" })
+})
+
+
+/// DELETE PURCHASE BY ID
+app.delete('/purchases/:id', async (req: Request, res: Response) => {
     
+    try {
+
+    const idToDelete = req.params.id
+
+    const [ purchase ] = await db("purchases").where({id: idToDelete})
+
+    if(!purchase){
+        res.status(400)
+        throw new Error ("Id não encontrado")
+    }
+
+    await db("purchases").del().where({id: idToDelete})
+
+    res.status(200).send("Compra deletada com sucesso")
+        
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+          } else {
+            res.status(500).send("Erro desconhecido.");
+          }
+    }
 })
